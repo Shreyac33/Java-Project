6f2aca19a2ad7cb0a05ccf3ba5feb139ac2d6e6e
RohanJibate <rohanjibate17@gmail.com>
2024-03-01 23:52:31 +0530

    wq
    exit
    git commit -m

---
+++ rohan.java
@@ -0,0 +1,20 @@
+case 3:
+                    if (studentCount == 0) {
+                        System.out.println("No students added yet!");
+                        break;
+                    }
+                    System.out.print("Name\t\t");
+                    for (int days=0;days<31;days++)
+                    {
+                       System.out.print("Day-"+(days+1)+"\t\t");
+                    }
+                    System.out.println();
+                    for (int i = 0; i < studentCount; i++) {
+                        System.out.print(students[i] + "\t");
+                        for (int j = 0; j < currentDay; j++) {
+                            System.out.print("  ");
+                            System.out.print(attendance[i][j] ? "\tPresent " : "\tAbsent ");
+                        }
+                        System.out.println();
+                    }
+                    break;
