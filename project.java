f205e1ecadbb0e6ad5d370e193131a01ffe8d52a
Anushacakalawadi <anushaakalawadi@gmail.com>
2024-03-02 11:50:27 +0530

    miniproject

---
+++ project.java
@@ -0,0 +1,26 @@
+case 2:
+                    if (studentCount == 0) {
+                        System.out.println("No students added yet!");
+                        break;
+                    }
+
+                    if (currentDay >= MAX_DAYS) {
+                        System.out.println("Maximum days limit reached for marking attendance!");
+                        break;
+                    }
+
+                    System.out.println("Marking attendance for day " + (currentDay + 1));
+                    for (int i = 0; i < studentCount; i++) {
+                        System.out.print("Is " + students[i] + " present on day"+(currentDay+1) +" (Y/N): ");
+                        String attendanceStatus = scanner.next().toUpperCase();
+                        if (attendanceStatus.equals("Y")) {
+                            attendance[i][currentDay] = true;
+                        } else if (attendanceStatus.equals("N")) {
+                            attendance[i][currentDay] = false;
+                        } else {
+                            System.out.println("Invalid input! Please enter Y or N.");
+                            i--;
+                        }
+                    }
+                    currentDay++;
+                    break;
