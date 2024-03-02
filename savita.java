553333029c7dc2db2bac3c8f1a0a0ba6e0ca4832
unknown <savitabc12@gmail.com>
2024-03-02 11:52:11 +0530

    frist savita

---
+++ savita.java
@@ -0,0 +1,20 @@
+ while (true) {
+            System.out.println("\n1. Add student");
+            System.out.println("2. Mark attendance");
+            System.out.println("3. View attendance");
+            System.out.println("4. Exit");
+            System.out.print("Enter your choice: ");
+            int choice = scanner.nextInt();
+
+            switch (choice) {
+                case 1:
+                    if (studentCount < MAX_STUDENTS) {
+                        System.out.print("Enter student name: ");
+                        String studentName = scanner.next();
+                        students[studentCount++] = studentName;
+                        System.out.println("Student added successfully!");
+                    } else {
+                        System.out.println("Maximum student limit reached!");
+                    }
+                    break;
+Z
